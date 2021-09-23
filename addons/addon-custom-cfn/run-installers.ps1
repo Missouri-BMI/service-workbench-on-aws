f1e5408d562d6dcf875fd164bfc463cb05c6d6a4
BuildTools <unconfigured@null.spigotmc.org>
2021-09-23 11:19:35 -0500

    changed syntax to make it work

--- addons/addon-custom-cfn/run-installers.ps1
+++ addons/addon-custom-cfn/run-installers.ps1
@@ -6,8 +6,8 @@ c:\workdir\rtools.exe /verysilent
 c:\workdir\snowflake64_odbc.msi /quiet
 cmd /c "c:\workdir\QualysCloudAgent.exe CustomerId={b288187c-6e29-d330-8389-5b291af9e73f} ActivationId={75beca74-1e31-49b0-a353-cb176a99d0ef} WebServiceUri=https://qagpublic.qg1.apps.qualys.com/CloudAgent/"
 c:\workdir\jre.exe /s
-c:\workdir\FirefoxInstaller.exe /S
-c:\workdir\ChromeSetup.exe /silent /install
+cmd /c "c:\workdir\FirefoxInstaller.exe /S"
+cmd /c "c:\workdir\ChromeSetup.exe /silent /install"
 
 #create shortcut to rstudio on desktop
 New-Item -ItemType SymbolicLink -Path C:\Users\Administrator\Desktop\ -Name "RStudio.lnk" -Value "C:\Program Files\RStudio\bin\rstudio.exe"
