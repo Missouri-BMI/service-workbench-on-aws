776b48726e2145b78b32418e7709f75118e0ab04
BuildTools <unconfigured@null.spigotmc.org>
2021-11-01 15:10:01 -0500

    adding git to path

--- addons/addon-custom-cfn/run-installers.ps1
+++ addons/addon-custom-cfn/run-installers.ps1
@@ -8,7 +8,7 @@ cmd /c "c:\workdir\QualysCloudAgent.exe CustomerId={b288187c-6e29-d330-8389-5b29
 c:\workdir\jre.exe /s
 c:\workdir\FirefoxSetup.exe /S
 cmd /c "c:\workdir\ChromeSetup.exe /silent /install"
-c:\workdir\PortableGit.7z.exe -o c:\users\administrator\git -y
+c:\workdir\PortableGit.7z.exe -o c:\git -y
 
 #create shortcut to rstudio on desktop
 New-Item -ItemType SymbolicLink -Path C:\Users\Administrator\Desktop\ -Name "RStudio.lnk" -Value "C:\Program Files\RStudio\bin\rstudio.exe"
@@ -19,3 +19,6 @@ Rename-LocalUser -name Guest -newname NotGuest
 #remove ec2 guide/feedback shortcuts
 rm 'C:\Users\Administrator\Desktop\EC2 Feedback.website'
 rm 'C:\Users\Administrator\Desktop\EC2 Microsoft Windows Guide.website'
+
+#add git to path
+setx path "%path%;C:\git\bin"
