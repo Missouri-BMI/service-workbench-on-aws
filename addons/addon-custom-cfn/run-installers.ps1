dbb84a260a692b1a1dce8ab648d662e7d5eda361
BuildTools <unconfigured@null.spigotmc.org>
2021-11-16 18:08:06 -0600

    reordered MSI files

--- addons/addon-custom-cfn/run-installers.ps1
+++ addons/addon-custom-cfn/run-installers.ps1
@@ -1,16 +1,16 @@
 #run all installers
+c:\workdir\snowflake64_odbc.msi /quiet
 cmd /c  "c:\workdir\GitHubDesktopSetup-x64.exe /s"
 c:\workdir\R.exe /verysilent
 c:\workdir\RStudio.exe /S
 c:\workdir\rtools.exe /verysilent
-c:\workdir\snowflake64_odbc.msi /quiet
-c:\workdir\AWSCLIV2.msi /quiet
 c:\workdir\python-3.10.exe /quiet PrependPath=1
 cmd /c "c:\workdir\QualysCloudAgent.exe CustomerId={b288187c-6e29-d330-8389-5b291af9e73f} ActivationId={75beca74-1e31-49b0-a353-cb176a99d0ef} WebServiceUri=https://qagpublic.qg1.apps.qualys.com/CloudAgent/"
 c:\workdir\jre.exe /s
 c:\workdir\FirefoxSetup.exe /S
 cmd /c "c:\workdir\ChromeSetup.exe /silent /install"
 c:\workdir\PortableGit.7z.exe -o c:\git -y
+c:\workdir\AWSCLIV2.msi /quiet
 
 #create shortcut to rstudio on desktop
 New-Item -ItemType SymbolicLink -Path C:\Users\Administrator\Desktop\ -Name "RStudio.lnk" -Value "C:\Program Files\RStudio\bin\rstudio.exe"
